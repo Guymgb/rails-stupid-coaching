35bf0733b9054ab5d26f278676a8915d69ab9e31
Guy Gan Baruch Guygb91@gmail.com <guygb91@gmail.com>
2022-05-16 17:13:15 +0100

    works

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-
+    @question = params[:question]
+    @answer = if @question.ends_with?("?")
+      "Silly question, get dressed and go to work!"
+    elsif @question == "I am going to work"
+      "Great!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 end
